254215b81b4e75f297b23d5e19e32c40d31e4610
Astro-afk <60247879+Astro-afk@users.noreply.github.com>
2022-08-12 22:34:42 -0700

    Create blindinglights.lua

---
+++ blindinglights.lua
@@ -0,0 +1,50 @@
+-- Sheet for Rondo Alla Turca by Wolfgang Amadeus Mozart
+
+shared.stop = true -- stops the player at any time if true
+-- CONFIG:
+shared.ftime = 2*60 + 10 -- time in seconds for the song to finish (extended by |)
+shared.delay = nil -- delay overides the ftime
+shared.tempo = 178 -- tempo overides the delay
+
+shared.scr = [[
+i i i |
+Yio t Y i i i |
+Yio t Y Po i Y Po i Y i |
+tEtEt |
+tEtEy EE tw |
+tEtE y EE tw |
+E t wq |
+EtEtEt |
+tEtEy EE tw |
+tEtEy EE tw |
+E t wq |
+EtEtE |
+t Yi o t Y i |
+t t Yi o t Y i |
+E t Yi o t Y i y t E |
+Et Y i |
+io i YY tY |
+Y Po i Y Po i Y i |
+o Y i |
+o i YY tY |
+Y Po i Y Po i Y i i i |
+Yio t Y i i i |
+Yio t Y Po i Y Po i Y i |
+EtEtEt |
+EtEtEy EE tw |
+tEtEy EE tw |
+E t wq |
+Y y t E t t Yi o t Y i |
+t t Yi o t Y i |
+E t Yi o t Y i y t E |
+Y Po i Y Po i Y i i i |
+PoPos oo ii |
+P s oo io |
+P P Ooi |
+o Y i |
+o i YY tY |
+Y Po i Y Po i Y i i i |
+Yi o t Y i i |
+Yio t Y Po i Y Po i Y i i i Yio t Y i |
+i Yio t Y Po i Y Po i Y i i | i
+]]
